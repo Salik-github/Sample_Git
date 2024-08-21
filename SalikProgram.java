7d82216f506add6fc474ed0b6c8500bc35befb95
Salik-github <114048639+Salik-github@users.noreply.github.com>
2024-08-21 22:42:23 +0530

    test

--- SalikProgram.java
+++ SalikProgram.java
@@ -1,8 +1,6 @@
 public class SalikProgram{
 public static void main (String [] args)
 {
-System.out.println("First Program one");
-System.out.println("tres");
-}
+System.out.println("First Program one");}
   
 }
